1dedd6b06584cdf840e6115fe500e162c3f0432a
AranJannson <me@aranjannson.com>
2025-02-21 11:58:56 +0000

    Added Frontend branch

--- NextJS (Frontend)/app/page.tsx
+++ NextJS (Frontend)/app/page.tsx
@@ -7,12 +7,16 @@ export default function Home() {
     return (
         <div className="flex flex-col gap-5 overflow-hidden bg-background-100">
             <div className="flex flex-col">
-                <div className="w-screen py-4 bg-accent-200 grid grid-cols-1 md:grid-cols-3 gap-5 items-center justify-center content-center md:px-0 px-4">
+                <div className="w-screen py-4  grid grid-cols-1 md:grid-cols-3 gap-5 items-center justify-center content-center md:px-0 px-4">
                     <HeroProduct url="https://dummyimage.com/500x350/fff/000.png" text="Temp1" />
                     <HeroProduct url="https://dummyimage.com/500x350/fff/000.png" text="Temp2" />
                     <HeroProduct url="https://dummyimage.com/500x350/fff/000.png" text="Temp3" />
+
                 </div>
                 <NavBar />
+
+                <div className="w-full bg-gradient-to-b from-primary-200 to-background-100 h-20"/>
+
                 <Featured />
             </div>
         </div>
