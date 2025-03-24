0066124eae0239fee132575b6b8ad8d1ce6bf442
AranJannson <me@aranjannson.com>
2025-03-24 15:44:01 +0000

    Setup products page to fetch data from Catalouge Microservice

--- Microservices/Catalog/src/utils/fetchCatalog.ts
+++ Microservices/Catalog/src/utils/fetchCatalog.ts
@@ -1,5 +1,35 @@
+import { connect } from "./dbConnect"
 
-export const fetchCatalog = async (): Promise<any> => {
+const supabase = connect();
 
-    return null;
+export async function fetchCatalouge(){
+
+    const { data } = await supabase.from("items").select("*");
+
+    return data;
+}
+
+
+export async function fetchOrgProducts(org_id: number){
+
+    const { data } = await supabase.from("items").select("*").eq("org_id", org_id);
+
+    return data;
+
+}
+
+export async function fetchStock(item_id: number) {
+
+    const { data } = await supabase.from("stock").select("*").eq("item_id", item_id).maybeSingle();
+
+    return data;
+
+}
+
+export async function fetchItemsBasedOnCategory(category: string){
+
+    const { data } = await supabase.from("items").select("*").eq("category", category);
+
+    return data;
+
 }
