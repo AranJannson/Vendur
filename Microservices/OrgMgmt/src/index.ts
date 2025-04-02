398e4278a3f8d12d1d7a651f187df5907fbd6881
Ismaeel Anjum <ismaeel26anjum@gmail.com>
2025-04-02 15:45:03 +0100

    Reinitialised OrgMgmt Microservice

--- Microservices/OrgMgmt/src/index.ts
+++ Microservices/OrgMgmt/src/index.ts
@@ -1,53 +1,21 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import {getRandomItemImages} from "./utils/displayImage";
+import searchOrgMgmtue from "./utils/search";
+import checkStock from "./utils/fetchIItemInfo";
+import { fetchAllReviews, reviews, makeReview } from "./utils/reviews";
 
 dotenv.config();
 
 const OrgMgmt = express();
 
 OrgMgmt.use(express.json());
+
 OrgMgmt.use(cors());
 
-const portNumber = 8003;
+const portNumber = 8000;
 
 OrgMgmt.listen(portNumber, () => {
     console.log(`OrgMgmt is running on port ${portNumber}`);
 });
 
-// OrgMgmt.get("/OrgGet", async (req: Request, res: Response) => {
-//     const db = await connect();
-//
-//     const itemsList = await db.from('items').select('*');
-//
-//     console.log(JSON.stringify(itemsList, null, 2));
-//
-//     res.send(JSON.stringify(itemsList, null, 2));
-// });
-//
-// OrgMgmt.post("/OrgPost", async (req: Request, res: Response): Promise<any> => {
-//
-//     const supabase = await connect();
-//
-//     await supabase.from('items').insert({
-//         name: 'Pyoro',
-//         image: "https://mario.wiki.gallery/images/5/5d/WWGIT_CS_Pyoro.png",
-//         price: 4000,
-//         description: "A cute little bird that eats bugs."
-//     });
-//
-// });
-
-OrgMgmt.get("/display-random-item-images", async (req: Request, res: Response): Promise<any> => {
-
-    const { imageCount } = req.body;
-
-    const data = await getRandomItemImages(imageCount);
-
-    if (!data) {
-        return res.status(500).send({ error: "Failed to fetch images" });
-    }
-
-    res.status(200).send(data);
-});
