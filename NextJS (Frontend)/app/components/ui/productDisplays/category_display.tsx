019b307b9b28300cbf653416b243b69c6d5e8370
Tom <madtomtmr@gmail.com>
2025-04-13 03:58:52 +0100

    very small fix to homepage catgories display

--- NextJS (Frontend)/app/components/ui/productDisplays/category_display.tsx
+++ NextJS (Frontend)/app/components/ui/productDisplays/category_display.tsx
@@ -57,7 +57,7 @@ export default async function category_display() {
                     Explore All Of Our Categories
                 </h2>
             </div>
-
+            
             <div className="grid md:grid-cols-10 grid-cols-1 gap-8 p-4 bg-background-100 rounded-b-xl">
                 {categories.map((category, catIndex) => (
                     <Link
@@ -66,26 +66,21 @@ export default async function category_display() {
                             query: { category: category.name, sort: "relevance" },
                         }}
                         key={catIndex}
-                        className="flex flex-col items-center text-center"
-                    >
-                        <h3 className="text-[15px] font-bold mb-1 text-[25px] min-h-[60px] flex items-center justify-center">{category.name}</h3>
-                        <div className="gap-4 bg-background-50 p-1 rounded-xl">
-                            <div className="relative group aspect-square rounded-xljustify-center items-center">
+                        className="flex flex-col items-center text-center pt-12 bg-background-200 rounded-xl">
+                        <h3 className="text-[15px] font-bold mb-1 text-[22px] h-[60px] flex items-center justify-center pb-10">{category.name}</h3>
+                        <div className="bg-background-50 p-1 rounded-xl">
+                            <div className="rounded-xl overflow-hidden w-full h-[350px] bg-white">
                                 <img
                                     src={category.src} 
                                     alt={category.alt} 
-                                    width="300"
-                                    height="200"
                                     style={{
-                                        width: '200px', 
-                                        height: '400px', 
-                                        objectFit: 'cover', 
                                         objectPosition: `${category.shiftPercentage} center`
                                     }}
-                                    className="rounded-lg"
+                                    className="rounded-lg w-full h-full object-cover"
                                 />
                             </div>
                         </div>
+                      
                     </Link>
                 ))}
             </div>
