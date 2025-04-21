ccee18f38fe14172e056afcc9e31fe02e750bcc6
Tom <madtomtmr@gmail.com>
2025-04-21 18:00:27 +0100

    Admin microservice conflict fixed

--- Microservices/Admin/src/utils/management.ts
+++ Microservices/Admin/src/utils/management.ts
@@ -1,24 +1,58 @@
-// import { items, orders } from '../drizzle/schema'
-// import { connect } from './dbConnect'
-// import { eq } from 'drizzle-orm';
+import { connect } from "./dbConnect"
 
-// export default async function viewOrders(){
+const supabase = connect();
 
-//     const db  = await connect();
+export async function deleteProductById(id: string): Promise<{ error: string | null }> {
+  
+    const { error } = await supabase.from('items').delete().eq('id', id);
+    console.log('(MANAGEMENT) Deleting product with id:', id);
+    if (error) {
+      return { error: error.message };
+    }
+  
+    return { error: null };
+  }
 
-//     const stock = await db!.select().from(orders)
+export async function getAllOrgs() {
 
-//     return(JSON.stringify(stock, null, 2))
+    const { data, error } = await supabase.from('organisations').select('*');
+  
+    if (error) {
+        return { error: error.message, data: null};
+        }
+    
+    return data;
+    }
 
-// }
+export async function orgDetails(vendur_id: string): Promise<{data: any, error: string | null }> {
+    console.log(vendur_id);
+    const { data, error } = await supabase.from("organisations").select("*").eq("id", vendur_id).single();
+    
+    if (error) {
+        console.error("Error fetching organisation details:", error.message);
+        return { data: null, error: error.message };
+      }
+    return { data, error: null };
+      }
 
-
-// export async function getItemInfo(item_id: number){
-
-//     const db  = await connect();
-
-//     const itemDetails = await db!.select().from(items).where(eq(items.item_id, item_id))
-
-//     return(JSON.stringify(itemDetails, null, 2))
-
-// }
+export async function updateOrganisationByID(id: string, updateData: {email: string, name: string, description: string, telephone: string, website: string, address: string }): Promise<{ data: any, error: string | null }> {
+    try {
+        const { email, name, description, telephone, website, address } = updateData;
+        const { data, error } = await supabase.from("organisations").update({
+            email,
+            name,
+            description,
+            telephone,
+            website,
+            address
+        }).eq("id", id);
+    
+        if (error) {
+        return { data: null, error: error.message };
+        }
+        return { data, error: null };
+    } catch (err) {
+        console.error("Unexpected error:", err);
+        return { data: null, error: "An unexpected error occurred" };
+    }
+      }
