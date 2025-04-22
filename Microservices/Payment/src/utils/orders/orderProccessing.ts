470e41073299187db38092780b34dd19d2eaa5d3
Alex Kyriacou <akyriacou08@gmail.com>
2025-04-22 14:48:10 +0100

    Placed order updates the order and order_group tables with items ordered and a group reference linking them

--- Microservices/Payment/src/utils/orders/orderProccessing.ts
+++ Microservices/Payment/src/utils/orders/orderProccessing.ts
@@ -2,41 +2,23 @@ import {createClient} from "@supabase/supabase-js";
 
 type Item = {
     id: string;
-    name: string;
-    price: number;
-    discount: number;
-    category: string;
-    org_id: string;
+    quantity: number;
 };
 
 export default async function orderProcessing(basket: Item[], user_id: string) {
 
     const supabase = createClient(process.env.PUBLIC_SUPABASE_URL as string, process.env.PUBLIC_SUPABASE_ANON_KEY as string);
 
-    const itemsLogMap = new Map<string, { item: Item; quantity: number }>
+    const groupId = supabase.from("group_orders").insert(user_id).select("id").single();
+
 
     for (const item of basket) {
-        const checkIfExists = itemsLogMap.get(item.id);
-        if (checkIfExists) {
-            checkIfExists.quantity += 1;
-        } else {
-            itemsLogMap.set(item.id, { item, quantity: 1 });
-        }
-    }
-
-    const itemIds = Array.from(itemsLogMap.values()).map(({ item }) => Number(item.id));
-    const quantities = Array.from(itemsLogMap.values()).map(({ quantity }) => quantity);
-
-    try{
-        const { error } = await supabase.from("orders").insert([{items: itemIds, quantity: quantities, user_id},]);
+        const { data, error } = await supabase.from("orders").insert([item.id, item.quantity, groupId, "pending"]);
 
         if (error) {
             console.error("Error inserting order:", error);
             return 0;
         }
-    }catch (error){
-        console.error("Error inserting order:", error);
-        return 0;
     }
 
     return 1;
