fea83ddb9452e41acd7aac6b3aa91c33e52ee78d
Tom <madtomtmr@gmail.com>
2025-04-27 15:09:36 +0100

    no changes

--- Microservices/OrgMgmt/src/utils/verification.ts
+++ Microservices/OrgMgmt/src/utils/verification.ts
@@ -2,19 +2,10 @@ import { connectCatalogue } from "./dbConnect";
 
 const supabase = connectCatalogue();
 
-export const requestVerification = async (org_id: number, name: string, email: string, description: string, productInfo: string, shippingMethod: string) => {
+export const requestVerification = async (org_id: any) => {
     const { data, error } = await supabase
         .from("verification_requests")
-        .insert([
-            {
-                org_id,
-                name,
-                email,
-                description,
-                productInfo,
-                shippingMethod
-            }
-        ]);
+        .insert([{ org_id }]);
 
     if (error) {
         throw new Error(`Error requesting verification: ${error.message}`);
