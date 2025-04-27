0e28d3cdd3d3ef5b40d993948bb5a890fd45a89c
AranJannson <me@aranjannson.com>
2025-04-27 18:51:25 +0100

    Added check for team id on org man page

--- NextJS (Frontend)/app/organisations/management/page.tsx
+++ NextJS (Frontend)/app/organisations/management/page.tsx
@@ -4,10 +4,30 @@ import AddProducts from "@/app/components/forms/AddProducts";
 import VerificationStatus from "@/app/components/org/VerificationStatus";
 import OrgAnalytics from "@/app/components/org/Analytics";
 import { stackServerApp } from "@/stack";
+import AddToOrg from "@/app/components/forms/AddToOrg";
 
 export default async function OrganisationsManagement() {
     const user = await stackServerApp.getUser({ or: 'redirect' });
     const allTeams = await user.listTeams();
+
+    if (allTeams.length === 0) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="flex flex-col gap-4 bg-primary-200 p-8 rounded-lg shadow-xl text-center">
+                    <h1 className="text-2xl font-bold ">
+                        Want To Sell Products?
+                    </h1>
+                    <h1>
+                        Create or Join an Organisation
+                    </h1>
+                    <Link href="/organisations/create" className="bg-secondary-400 rounded-lg p-2 transition-colors hover:bg-secondary-500">
+                        Create!
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+
 
     return(
         <>
@@ -15,9 +35,9 @@ export default async function OrganisationsManagement() {
 
                 <div className="flex flex-col gap-8">
 
-                    <Link href="#" className="w-full h-[10vh] bg-primary-200 rounded-lg shadow-xl transition-colors hover:bg-primary-300 text-center flex items-center justify-center font-bold text-2xl">
-                        Manage Listings
-                    </Link>
+                    <h1  className="w-full h-[10vh] bg-primary-200 rounded-lg shadow-xl  text-center flex items-center justify-center font-bold text-2xl">
+                        {allTeams[0]?.displayName} - Dashboard
+                    </h1>
 
                     <Products/>
 
@@ -43,7 +63,7 @@ export default async function OrganisationsManagement() {
                 </div>
 
                 <div className="w-full h-20 bg-primary-200 rounded-lg shadow-xl col-span-3">
-
+                    {/*<AddToOrg/>*/}
                 </div>
 
             </div>
