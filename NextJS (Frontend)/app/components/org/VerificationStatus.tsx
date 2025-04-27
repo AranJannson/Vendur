1ddc9e06ba1082a2723adc08ede64ff76a831199
Tom <madtomtmr@gmail.com>
2025-04-27 03:42:11 +0100

    Added org verification check on management page

--- NextJS (Frontend)/app/components/org/VerificationStatus.tsx
+++ NextJS (Frontend)/app/components/org/VerificationStatus.tsx
@@ -1,32 +1,55 @@
 'use client'
 import VerificationTick from "@/app/components/ui/VerificationTick";
+import { useState, useEffect } from 'react';
 
-const checkIfVerified = 0; // Replace with actual verification check
+const org_id = 4; //TEMPORARY ORG ID! (Set in DB to)
 
-export default function VerificationStatus() {
-
+const VerificationStatus = () => {
 
     const handleVerificationRequest = async () => {
         alert("Verification request sent");
     }
 
+    const [checkIfVerified, setCheckIfVerified] = useState<boolean | null>(null);
+
+    
+        const fetchData = async () => {
+          try {
+            const res = await fetch('/api/organisations/verification/check', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ org_id }),
+              });  
+                const data = await res.json();
+                setCheckIfVerified(data.verified);
+
+          } catch (error) {
+            console.error("Error fetching data:", error);
+            setCheckIfVerified(false);
+            }
+        };
+
+        useEffect(() => {
+            fetchData();
+        }, []);
+
     return (
         <div className="bg-primary-100 shadow-xl rounded-xl p-4 h-fit">
             <h1 className="font-bold text-xl mb-2">Verification Status</h1>
-
-            {checkIfVerified ? (
+            {checkIfVerified === null ? (
+                <p>Loading...</p>
+            ) : checkIfVerified ? (
                 <VerificationTick />
             ) : (
-                <div className="flex flex-col gap-2">
-
-                    <p className="text-red-500 font-medium">Not Verified</p>
-
-                    <button onClick={handleVerificationRequest} className="bg-secondary-300 p-3 font-semibold rounded-xl transition-colors hover:bg-secondary-400">Request Verification</button>
-
-                </div>
-
-
+                <p className="text-red-500 font-medium">Not Verified</p>
             )}
+            <div className="flex flex-col gap-2">
+                <button onClick={handleVerificationRequest} className="bg-secondary-300 p-3 font-semibold rounded-xl transition-colors hover:bg-secondary-400">Request Verification</button>
+            </div>
+            
         </div>
     );
-}
+};
+export default VerificationStatus;
