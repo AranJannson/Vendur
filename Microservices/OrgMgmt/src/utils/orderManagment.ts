8019f31bf55470c68c7ee5c88e4262d1fe7e1401
Ismaeel Anjum <ismaeel26anjum@gmail.com>
2025-05-15 15:12:07 +0100

    Added Order management functions

--- Microservices/OrgMgmt/src/utils/orderManagment.ts
+++ Microservices/OrgMgmt/src/utils/orderManagment.ts
@@ -1,5 +1,21 @@
 import { connectPayment } from "./dbConnect";
 
+// TODO: Get All Orders for a specific org
+export async function getAllOrders(org_id: string) {
+  const supabase = connectPayment();
+  const { data, error } = await supabase
+    .from("orders")
+    .select("*")
+    .eq("org_id", org_id);
+
+  if (error) {
+    throw new Error(`Error fetching orders: ${error.message}`);
+  }
+
+  return data;
+}
+
+// TODO: Get Order Group by ID
 export async function getOrderById(orderId: string) {
   const supabase = connectPayment();
   const { data, error } = await supabase
@@ -14,9 +30,32 @@ export async function getOrderById(orderId: string) {
 
   return data;
 }
-
-// TODO: Get Order Group by ID
-
 // TODO: Change status of order e.g. "pending" to "completed"
+export async function updateOrderStatus(orderId: string, status: string) {
+    const supabase = connectPayment();
+    const { data, error } = await supabase
+        .from("orders")
+        .update({ status })
+        .eq("id", orderId);
+
+    if (error) {
+        throw new Error(`Error updating order status: ${error.message}`);
+    }
+
+    return data;
+}
 
 // TODO: Delete/cancel an order
+export async function deleteOrder(orderId: string) {
+    const supabase = connectPayment();
+    const { data, error } = await supabase
+        .from("orders")
+        .delete()
+        .eq("id", orderId);
+
+    if (error) {
+        throw new Error(`Error deleting order: ${error.message}`);
+    }
+
+    return data;
+}
