94a5e8a5ed560564a297435667f1b806af11506b
AranJannson <me@aranjannson.com>
2025-05-18 22:55:43 +0100

    Migrating add product to use next api route

---
+++ NextJS (Frontend)/app/api/getCreateProduct/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const { payload } = await req.json();
+        const response = await fetch("http://localhost:8003/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                payload
+            })
+        });
+
+        const data = await response.json();
+
+        return NextResponse.json(data)
+    } catch (error) {
+        console.error("Error in getOrderByOrg API:", error);
+        return Response.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
