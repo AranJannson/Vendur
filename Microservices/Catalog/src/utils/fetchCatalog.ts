3116dfbd5ee224ad08689554c60f310cd67cb359
AranJannson <me@aranjannson.com>
2025-05-19 22:39:20 +0100

    Changed the way the Catalog MS access the database on fetchCatalog

--- Microservices/Catalog/src/utils/fetchCatalog.ts
+++ Microservices/Catalog/src/utils/fetchCatalog.ts
@@ -1,6 +1,7 @@
 import { connect } from "./dbConnect"
+import {createClient} from "@supabase/supabase-js";
 
-const supabase = connect();
+let supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
 
 export async function fetchCatalouge(){
 
