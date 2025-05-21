47b04325b53f8e02069bb44a5f64ce83645a9fda
Ismaeel Anjum <ismaeel26anjum@gmail.com>
2025-05-21 21:21:36 +0100

    Working on GetBasket

--- NextJS (Frontend)/app/components/payment/GetBasket.tsx
+++ NextJS (Frontend)/app/components/payment/GetBasket.tsx
@@ -6,18 +6,26 @@ import {useEffect, useState} from "react";
 
 interface Basket{
 
-    user_id: string,
-    created_at: Date,
-    items: number[],
-    quantities: number[]
+    user_id: string;
+    created_at: Date;
+    items: number[];
+    quantities: number[];
 
+}
+
+interface Item {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
 }
 
 export default function GetBasket() {
 
     const userId = useUser()?.id
-    
+
     const [basket, setBasket] = useState<Basket>();
+    const [itemDetails, setItemDetails] = useState<Item[]>([]);
     useEffect(() => {
         const fetchBasket = async () => {
             const response = await fetch("/api/getBasketNew",
@@ -37,15 +45,21 @@ export default function GetBasket() {
         };
 
         fetchBasket();
-    }, []);
-
+    }, [userId]);
 
 
 
     return(
 
         <div>
-            <code>{JSON.stringify(basket)}</code>
+            <code>{JSON.stringify(basket, null, 1)}</code>
+            {itemDetails.map(item => (
+                <div key={item.id} className="flex flex-col items-center justify-center">
+                    <img src={item.image} alt={item.name} className="w-32 h-32" />
+                    <h2>{item.name}</h2>
+                    <p>Price: ${item.price}</p>
+                </div>
+            ))}
         </div>
 
     );
