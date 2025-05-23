d1fdefa33f1edb935f27edc9f9559446262f08ee
Ismaeel Anjum <ismaeel26anjum@gmail.com>
2025-05-23 13:47:51 +0100

    Fixed issue where is_verified not in payload for edit org

--- NextJS (Frontend)/app/api/admin/orgEdit/route.ts
+++ NextJS (Frontend)/app/api/admin/orgEdit/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(req: NextRequest) {
-      const { id, name, email, description, telephone, website, address } = await req.json();
+      const { id, name, email, description, telephone, website, address, is_verified } = await req.json();
       const backendResponse = await fetch('http://localhost:5078/admin/editOrgDetails', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id, name, email, description, telephone, website, address }),
+        body: JSON.stringify({ id, name, email, description, telephone, website, address, is_verified }),
       });
       const data = await backendResponse.json();
       return NextResponse.json(data, { status: 200 });
