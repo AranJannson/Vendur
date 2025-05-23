ec7d1235e3572a1af1109913b2d7563820036d33
AranJannson <me@aranjannson.com>
2025-05-23 12:00:27 +0100

    Added check if user is admin to access admin panel

--- NextJS (Frontend)/app/admin/layout.tsx
+++ NextJS (Frontend)/app/admin/layout.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from 'react';
 import {AdminHeader} from "@/app/components/admin/AdminHeader";
+import {stackServerApp} from "@/stack";
 
 export const metadata = {
   title: "Admin | Vendur",
   description: "",
 };
 
-export default function Layout({
+export default async function Layout({
   children,
   analytics,
   products,
@@ -19,6 +20,20 @@ export default function Layout({
   verification: ReactNode
   organisations: ReactNode
 }) {
+
+
+    const user = await stackServerApp.getUser({ or: 'redirect' });
+    const userTeam = await user.getTeam("b523f683-3c55-4341-977d-6252a77265c1");
+
+    if(!userTeam){
+        return (
+            <div className="flex flex-col items-center justify-center h-screen">
+                <h1 className="text-2xl font-bold">You are not an admin</h1>
+                <p className="text-lg">Please contact your administrator.</p>
+            </div>
+        )
+    }
+
   return (
     <>
         <AdminHeader/>
